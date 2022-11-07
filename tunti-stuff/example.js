77b6bd84d7d942a0d602534562faf03cf670f7dd
Mursuxb <mursuxb@gmail.com>
2022-11-07 11:16:29 +0200

    Initial commit

---
+++ tunti-stuff/example.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log('I\'m alive biiiitch');
+console.log('Suck on them tiddies like they udders');
+// alert('You have 60 viruses on your computer!!! To fix the issue call to 555 678 004');
+
---
+++ tunti-stuff/example1.js
@@ -0,0 +1,9 @@
+'use strict';
+// let ageInt = 23;
+// let ageStr = ageInt.toString();
+// ageStr = ageStr + 20;
+// document.querySelector('#p1').innerHTML = ageStr;
+// document.querySelector('#p2').innerHTML = ageInt;
+name = prompt('Gimme Name');
+document.querySelector('#p3').innerHTML = "Welcome " + name + "!";
+
