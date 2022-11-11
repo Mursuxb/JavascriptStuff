57079477e4b8e5977e55cff37f193db78e51667c
Mursuxb <mursuxb@gmail.com>
2022-11-11 11:14:37 +0200

    Done

--- Homework/Moduuli2/Mod2T1.js
+++ Homework/Moduuli2/Mod2T1.js
@@ -1,9 +1,8 @@
 'use strict';
-// num1 = prompt("Gimme First Number")
-// num2 = prompt("Gimme Second Number")
-// num3 = prompt("Gimme Third Number")
-// num4 = prompt("Gimme Fourth Number")
-// num5 = prompt("Gimme Fifth Number")
+// Write a program that prompts the user for five numbers and prints them in the reverse order they were entered. Print the result to the console.(2p)
+//  Save the numbers to an array, then use for-loop to iterate in reverse order.
+//  Do not use array.reverse() function.
+
 let numlist = [];
 let reversed = [];
 for (let i = 1; i <= 5; i++) {
