bb9066ee7ee338d809a9b908edca2913fb2b3cd3
Mursuxb <mursuxb@gmail.com>
2022-11-11 11:31:18 +0200

    työn alla

---
+++ Homework/Moduuli2/Mod2T2.js
@@ -0,0 +1,4 @@
+'use strict';
+// Write a program that asks the user for the number of participants.
+// After this, the program asks for the names of all participants.
+// Finally, the program prints the names of the participants on the web page in an ordered list (<ol>) in the same order as they were entered. (2p)
